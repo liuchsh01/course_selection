b9a3fb9d23f58f42392559adca30f46b2ea7deaa
zhangxiongfeng <619728459@qq.com>
2016-12-03 23:22:57 +0800

    增加相应的Dao、DaoImpl、Service、ServiceImpl

---
+++ src/main/java/com/course/selection/service/CourseService.java
@@ -0,0 +1,8 @@
+package com.course.selection.service;
+
+import com.course.selection.common.base.Service;
+import com.course.selection.entity.Course;
+
+public interface CourseService extends Service<Course>{
+
+}
