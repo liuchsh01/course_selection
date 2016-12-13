9f60fa88c7f678a65f00b2dbd55be1551e0d6e23
liuchsh01 <liuchangsheng@email.szu.edu.cn>
2016-12-13 23:18:13 +0800

    删除测试文件

--- src/main/java/com/course/selection/service/AService.java
+++
@@ -1,8 +0,0 @@
-package com.course.selection.service;
-
-import com.course.selection.common.base.Service;
-import com.course.selection.entity.A;
-
-public interface AService extends Service<A> {
-
-}
